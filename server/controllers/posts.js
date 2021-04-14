4267ca6408c91e6a361694b6403f8fe345a44940
geliliojlb7 <geliliojohnlloydb7@gmail.com>
2021-04-14 17:54:28 +0800

    part1-2

---
+++ server/controllers/posts.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const body = req.body;
+    const newPost = new PostMessage(body);
+
+    try {
+        await newPost.save();
+
+        res.status(200).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    try {
+        if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+    
+        res.json(updatedPost);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    
+        await PostMessage.findByIdAndRemove(id);
+
+        console.log('DElete')
+
+        res.json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    
+        const post = await PostMessage.findById(id);
+        const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+        res.json(updatedPost);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
